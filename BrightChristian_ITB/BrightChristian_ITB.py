40cb774cf0f1cd5b7f4151ae4724c25308d551ac
BrightChristian <92179298+BrightChristian@users.noreply.github.com>
2021-10-09 07:12:57 +0700

    Delete BrightChristian_ITB directory

--- BrightChristian_ITB/BrightChristian_ITB.py
+++
@@ -1,3 +0,0 @@
-print("Nama : Bright Christian Kusuma")
-print("Jurusan : STEI ITB")
-print("Alasan ingin masuk GDSC : ingin menambah pengetahuan dan mengembangkan minat saya dalam bidang teknologi informasi.")
